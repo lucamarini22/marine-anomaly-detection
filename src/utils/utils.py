fc4e5365de37a483a3c77028617ab387e116b65b
Luca <lucamarini1998@gmail.com>
2023-02-11 15:04:46 +0000

    update

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,6 +1,6 @@
 def get_patch_name_from_prediction_name(pred_name: str) -> str:
-    return "_".join(pred_name.split("_")[:-1])
+    return "_".join(pred_name.split("_"))  # [:-1])
 
 
 def get_tile_name_from_prediction_name(pred_name: str) -> str:
-    return "_".join(pred_name.split("_")[:-2])
+    return "_".join(pred_name.split("_"))  # [:-2])
