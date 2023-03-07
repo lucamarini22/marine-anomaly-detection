a1b0cd201d37e53a3c6e4ef6ec596b165347fed8
Luca Marini <lucamarini1998@gmail.com>
2023-03-07 21:22:40 +0000

    add constant file

---
+++ src/utils/constants.py
@@ -0,0 +1,17 @@
+# MARIDA excluded bands B09 and B10 => bands to use to do keypoints matching:
+# B01, B02, B03, B04, B05, B06, B07, B08, B8A, B11, B12
+BAND_NAMES_IN_MARIDA = [
+    "B01",
+    "B02",
+    "B03",
+    "B04",
+    "B05",
+    "B06",
+    "B07",
+    "B08",
+    "B8A",
+    "B11",
+    "B12",
+]
+
+NOT_TO_CONSIDER_MARIDA = ("cl", "conf")
