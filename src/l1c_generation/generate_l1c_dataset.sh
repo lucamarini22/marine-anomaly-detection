93bca9b25dbdc8c6c76c4ddd24965a7ae450f005
Luca Marini <lucamarini1998@gmail.com>
2023-03-10 14:02:36 +0000

    add script to automatically generate l1c dataset

--- src/l1c_generation/generate_l1c_dataset.sh
+++ src/l1c_generation/generate_l1c_dataset.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-python ./save_cophub_and_marida_patches_bands_2_png.py
+python save_cophub_and_marida_patches_bands_2_png.py \
+--marida_patches_path="/data/anomaly-marine-detection/data/patches/" \
+--cop_hub_patches_path="/data/pyraws_luca/pyraws/generate_l1c/l1c_images" \
+--pairs_file_path="/data/anomaly-marine-detection/src/l1c_generation/keypoints_pairs/cop_hub_marida_pairs.txt" \
+--output_folder_path="/data/anomaly-marine-detection/data/l1c_copernicus_hub/images_before_keypoint_matching/"
 
-git clone https://github.com/magicleap/SuperGluePretrainedNetwork
+DIRECTORY="/data/anomaly-marine-detection/src/l1c_generation/SuperGluePretrainedNetwork"
+if [ ! -d "$DIRECTORY" ]; then
+  git clone https://github.com/magicleap/SuperGluePretrainedNetwork
+fi
 
-./SuperGluePretrainedNetwork/match_pairs.py --input_pairs="./keypoints_pairs/cop_hub_marida_pairs.txt" --input_dir="/data/anomaly-marine-detection/data/l1c_copernicus_hub/images_before_keypoint_matching" --output_dir="./keypoints_pairs" --resize=-1 --superglue="outdoor" --max_keypoints=1024 --keypoint_threshold=0.015 --nms_radius=4 --match_threshold=0.75
+./SuperGluePretrainedNetwork/match_pairs.py \
+--input_pairs="./keypoints_pairs/cop_hub_marida_pairs.txt" \
+--input_dir="/data/anomaly-marine-detection/data/l1c_copernicus_hub/images_before_keypoint_matching" \
+--output_dir="./keypoints_pairs" \
+--resize=-1 \
+--superglue="outdoor" \
+--max_keypoints=1024 \
+--keypoint_threshold=0.015 \
+--nms_radius=4 \
+--match_threshold=0.75
 
-python shift_and_crop_cop_hub_images.py
+python shift_and_crop_cop_hub.py \
+--path_keypoints_folder="/data/anomaly-marine-detection/src/l1c_generation/keypoints_pairs" \
+--cop_hub_png_input_imgs_path="/data/anomaly-marine-detection/data/l1c_copernicus_hub/images_before_keypoint_matching/" \
+--cop_hub_png_output_imgs_path="/data/anomaly-marine-detection/data/l1c_copernicus_hub/images_after_keypoint_matching/"
 
-python save_shifted_and_cropped_bands_patches_2_tif.py
+python save_shifted_and_cropped_bands_patches_2_tif.py \
+--marida_file_path="/data/anomaly-marine-detection/data/patches/S2_1-12-19_48MYU/S2_1-12-19_48MYU_0.tif" \
+--bands_images_folder_path="/data/anomaly-marine-detection/data/l1c_copernicus_hub/images_after_keypoint_matching" \
+--out_folder_tif="/data/anomaly-marine-detection/data/l1c_copernicus_hub/tif_final"
