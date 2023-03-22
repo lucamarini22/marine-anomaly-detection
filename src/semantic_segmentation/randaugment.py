solved!!! now rotation works (bug np.reshape). Modify other augmentations now

src/semantic_segmentation/randaugment.py
@@ -96,11 +96,11 @@ def Equalize(img, **kwarg):
 
 
 def Identity(img, **kwarg):
-    prev_shape = img.shape
-    if img.shape[0] != img.shape[1]:
-        img = np.moveaxis(img, 0, -1)
+    # prev_shape = img.shape
+    # if img.shape[0] != img.shape[1]:
+    #    img = np.moveaxis(img, 0, -1)
     aug = img
-    return np.reshape(aug, prev_shape)
+    return aug  # np.reshape(aug, prev_shape)
 
 
 def Posterize(img, v, max_v, bias=0):
@@ -120,7 +120,10 @@ def Rotate(img, v, max_v, bias=0):
     if random.random() < 0.5:
         v = -v
     aug = A.rotate(img, v)
-    return np.reshape(aug, prev_shape)
+    # np.reshape(aug, prev_shape)[8, :, :] # TODO returning np.reshape was the problem!!! Modify also other augmentations
+    if img.shape != prev_shape:
+        aug = np.moveaxis(aug, -1, 0)
+    return aug
 
 
 def Sharpness(img, v, max_v, bias=0):
@@ -283,13 +286,13 @@ def fixmatch_augment_pool():
         # (Color, 0.9, 0.05),
         # (Contrast, 0.9, 0.05),
         # (Equalize, None, None),
-        (Identity, None, None),
+        # (Identity, None, None),
         # (Posterize, 4, 4),
         (Rotate, 30, 0),
-        (Sharpness, 0.9, 0.05),
-        (ShearX, 0.3, 0),
-        (ShearY, 0.3, 0),
-        (Solarize, 256, 0),
+        # (Sharpness, 0.9, 0.05),
+        # (ShearX, 0.3, 0),
+        # (ShearY, 0.3, 0),
+        # (Solarize, 256, 0),
         # (TranslateX, 0.3, 0),
         # (TranslateY, 0.3, 0),
     ]

src/semantic_segmentation/train.py
@@ -329,10 +329,10 @@ def main(options):
     # TODO: modify class_distr when using ssl
     # (because you take a percentage of labels so the class distr of pixels
     # will change)
-    alphas = 1 - class_distr
-    # alphas = torch.Tensor(
-    #    [0.25, 1]
-    # )  # 0.25 * torch.ones_like(class_distr)  # 1 / class_distr
+    # alphas = 1 - class_distr
+    alphas = torch.Tensor(
+        [1, 0.25]
+    )  # 0.25 * torch.ones_like(class_distr)  # 1 / class_distr
     # alphas = alphas / max(alphas)  # normalize
     criterion = FocalLoss(
         alpha=alphas.to(device),
@@ -566,14 +566,20 @@ def main(options):
                     img_x, seg_map = next(labeled_iter)
 
                 try:
-                    img_u_w, img_u_s = next(
-                        unlabeled_iter
-                    )  # (img_u_w, img_u_s), _ = next(unlabeled_iter)
+                    img_u_w, img_u_s = next(unlabeled_iter)
                 except:
                     unlabeled_iter = iter(unlabeled_train_loader)
                     img_u_w, img_u_s = next(unlabeled_iter)
 
                 seg_map = seg_map.to(device)
+
+                for i in range(img_x.shape[0]):
+                    a = img_x[i, 8, :, :]
+                    b = seg_map[i, :, :].float()
+
+                    c = img_u_w[i, 8, :, :]
+                    d = img_u_s[i, 8, :, :]
+                    print()
 
                 # TODO: when deploying code to satellite hw, see if it's
                 # faster to put everything to device and make one single
@@ -817,7 +823,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--aggregate_classes",
         choices=list(CategoryAggregation),
-        default=CategoryAggregation.MULTI.value,
+        default=CategoryAggregation.BINARY.value,
         type=str,
         help="Aggregate classes into:\
             multi (Marine Water, Algae/OrganicMaterial, Marine Debris, Ship, and Cloud);\