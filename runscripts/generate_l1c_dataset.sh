617ca1cd83f2be9e8cddf9fcef116b755972c208
Luca Marini <lucamarini1998@gmail.com>
2023-04-03 10:48:47 +0000

    move file

--- runscripts/generate_l1c_dataset.sh
+++ runscripts/generate_l1c_dataset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python save_cophub_and_marida_patches_bands_2_png.py \
+python ../anomalymarinedetection/l1c_generation/save_cophub_and_marida_patches_bands_2_png.py \
 --marida_patches_path="/data/anomaly-marine-detection/data/patches/" \
 --cop_hub_patches_path="/data/pyraws_luca/pyraws/generate_l1c/l1c_images" \
 --pairs_file_path="/data/anomaly-marine-detection/data/l1c_data/keypoints_pairs/cop_hub_marida_pairs.txt" \
@@ -11,7 +11,7 @@ if [ ! -d "$DIRECTORY" ]; then
   git clone https://github.com/magicleap/SuperGluePretrainedNetwork
 fi
 
-./SuperGluePretrainedNetwork/match_pairs.py \
+../anomalymarinedetection/l1c_generation/SuperGluePretrainedNetworkSuperGluePretrainedNetwork/match_pairs.py \
 --input_pairs="/data/anomaly-marine-detection/data/l1c_data/keypoints_pairs/cop_hub_marida_pairs.txt" \
 --input_dir="/data/anomaly-marine-detection/data/l1c_data/images_before_keypoint_matching" \
 --output_dir="/data/anomaly-marine-detection/data/l1c_data/keypoints_pairs" \
@@ -22,12 +22,12 @@ fi
 --nms_radius=4 \
 --match_threshold=0.75 || exit
 
-python shift_and_crop_cop_hub.py \
+python ../scripts/shift_and_crop_cop_hub.py \
 --path_keypoints_folder="/data/anomaly-marine-detection/data/l1c_data/keypoints_pairs" \
 --cop_hub_png_input_imgs_path="/data/anomaly-marine-detection/data/l1c_data/images_before_keypoint_matching/" \
 --cop_hub_png_output_imgs_path="/data/anomaly-marine-detection/data/l1c_data/images_after_keypoint_matching/" || exit
 
-python save_shifted_and_cropped_bands_patches_2_tif.py \
+python ../scripts/save_shifted_and_cropped_bands_patches_2_tif.py \
 --marida_file_path="/data/anomaly-marine-detection/data/patches/S2_1-12-19_48MYU/S2_1-12-19_48MYU_0.tif" \
 --bands_images_folder_path="/data/anomaly-marine-detection/data/l1c_data/images_after_keypoint_matching" \
 --out_folder_tif="/data/anomaly-marine-detection/data/l1c_data/tif_final" || exit
