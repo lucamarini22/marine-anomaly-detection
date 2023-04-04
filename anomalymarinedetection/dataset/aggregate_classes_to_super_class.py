b338d190553dfefb7944b13cfabff6beff334156
Luca Marini <lucamarini1998@gmail.com>
2023-04-04 15:48:52 +0000

    add script

---
+++ anomalymarinedetection/dataset/aggregate_classes_to_super_class.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def aggregate_classes_to_super_class(
+    seg_map: np.ndarray,
+    classes_names_to_aggregate: list[str],
+    super_class_name: str,
+    cat_mapping_old: dict[str, int],
+    cat_mapping_new: dict[str, int],
+) -> np.ndarray:
+    """Change the values of pixels of image corresponding to class ids
+    included in classes_names_to_aggregate to the class id of
+    super_class_name.
+
+    Args:
+        seg_map (np.ndarray): segmentation map.
+        classes_names_to_aggregate (list[str]): list of names of the
+            classes to aggregate.
+        super_class_name (str): name of the class that aggregates other
+            classes.
+        cat_mapping_old (dict[str, int]): dictionary that maps old class
+            names to their class ids.
+        cat_mapping_new (dict[str, int]): dictionary that maps updated
+            class names to their updated class ids.
+
+    Returns:
+        np.ndarray: updated image.
+    """
+    for class_name in classes_names_to_aggregate:
+        seg_map[seg_map == cat_mapping_old[class_name]] = cat_mapping_new[
+            super_class_name
+        ]
+    return seg_map
--- anomalymarinedetection/dataset/dataloader.py
+++ anomalymarinedetection/dataset/dataloader.py
@@ -8,8 +8,6 @@ import torch
 import random
 import numpy as np
 from tqdm import tqdm
-from osgeo import gdal
-from os.path import dirname as up
 from torch.utils.data import Dataset
 
 from anomalymarinedetection.utils.assets import (
@@ -29,6 +27,10 @@ from anomalymarinedetection.io.load_data import (
 from anomalymarinedetection.dataset.categoryaggregation import (
     CategoryAggregation,
 )
+from anomalymarinedetection.dataset.aggregate_classes_to_super_class import (
+    aggregate_classes_to_super_class,
+)
+from anomalymarinedetection.dataset.get_roi_tokens import get_roi_tokens
 
 random.seed(0)
 np.random.seed(0)
@@ -40,31 +42,6 @@ class DataLoaderType(Enum):
     TRAIN_SSL = "train_ssl"
     VAL = "val"
     TEST = "test"
-
-
-def get_labeled_and_unlabeled_rois(
-    perc_labeled: float, path: str
-) -> tuple[list[str], list[str]]:
-    """Gets lists of regions of interests of labeled and unlabeled training
-    set.
-
-    Args:
-        perc_labeled (float): percentage of labeled data to use.
-        path (str, optional): path to dataset.
-
-    Returns:
-        tuple[list[str], list[str]]: list of names of labeled rois and list of
-          names of unlabeled rois.
-    """
-    # Semi-Supervised Learning (SSL)
-    ROIs = load_roi(os.path.join(path, "splits", "train_X.txt"))
-    num_unlabeled_samples = round(len(ROIs) * (1 - perc_labeled))
-    # Unlabeled regions of interests
-    ROIs_u = np.random.choice(ROIs, num_unlabeled_samples, replace=False)
-    # Labeled regions of interests
-    ROIs = np.setdiff1d(ROIs, ROIs_u)
-
-    return ROIs, ROIs_u
 
 
 class AnomalyMarineDataset(Dataset):
@@ -127,7 +104,7 @@ class AnomalyMarineDataset(Dataset):
             for roi in tqdm(
                 self.ROIs, desc="Load unlabeled train set to memory"
             ):
-                roi_file_path, _ = self.get_roi_tokens(path, roi)
+                roi_file_path, _ = get_roi_tokens(path, roi)
                 patch = load_patch(roi_file_path)
                 self.X_u.append(patch)
 
@@ -140,7 +117,7 @@ class AnomalyMarineDataset(Dataset):
                 self.ROIs, desc="Load labeled " + mode + " set to memory"
             ):
                 # Gets patch path and its semantic segmentation map path
-                roi_file_path, roi_file_cl_path = self.get_roi_tokens(path, roi)
+                roi_file_path, roi_file_cl_path = get_roi_tokens(path, roi)
                 # Loads semantic segmentation map
                 seg_map = load_segmentation_map(roi_file_cl_path)
 
@@ -160,7 +137,7 @@ class AnomalyMarineDataset(Dataset):
                         + 1
                     ]
                     super_organic_material_class_name = labels_multi[1]
-                    seg_map = self.aggregate_classes_to_super_class(
+                    seg_map = aggregate_classes_to_super_class(
                         seg_map,
                         algae_classes_names,
                         super_organic_material_class_name,
@@ -171,7 +148,7 @@ class AnomalyMarineDataset(Dataset):
                     # Aggregate Ship to new position
                     ship_class_name = [labels[4]]
                     super_ship_class_name = labels[4]
-                    seg_map = self.aggregate_classes_to_super_class(
+                    seg_map = aggregate_classes_to_super_class(
                         seg_map,
                         ship_class_name,
                         super_ship_class_name,
@@ -182,7 +159,7 @@ class AnomalyMarineDataset(Dataset):
                     # Aggregate Clouds to new position
                     clouds_class_name = [labels[5]]
                     super_clouds_class_name = labels[5]
-                    seg_map = self.aggregate_classes_to_super_class(
+                    seg_map = aggregate_classes_to_super_class(
                         seg_map,
                         clouds_class_name,
                         super_clouds_class_name,
@@ -196,7 +173,7 @@ class AnomalyMarineDataset(Dataset):
                     water_classes_names = labels[-9:]
                     super_water_class_name = labels[6]
 
-                    seg_map = self.aggregate_classes_to_super_class(
+                    seg_map = aggregate_classes_to_super_class(
                         seg_map,
                         water_classes_names,
                         super_water_class_name,
@@ -212,7 +189,7 @@ class AnomalyMarineDataset(Dataset):
                     super_class_name = labels_binary[
                         labels_binary.index("Other")
                     ]
-                    seg_map = self.aggregate_classes_to_super_class(
+                    seg_map = aggregate_classes_to_super_class(
                         seg_map,
                         other_classes_names,
                         super_class_name,
@@ -289,70 +266,4 @@ class AnomalyMarineDataset(Dataset):
                 img = self.standardization(img)
 
             return img, target
-
-    def aggregate_classes_to_super_class(
-        self,
-        seg_map: np.ndarray,
-        classes_names_to_aggregate: list[str],
-        super_class_name: str,
-        cat_mapping_old: dict[str, int],
-        cat_mapping_new: dict[str, int],
-    ) -> np.ndarray:
-        """Change the values of pixels of image corresponding to class ids
-        included in classes_names_to_aggregate to the class id of
-        super_class_name.
-
-        Args:
-            seg_map (np.ndarray): segmentation map.
-            classes_names_to_aggregate (list[str]): list of names of the
-              classes to aggregate.
-            super_class_name (str): name of the class that aggregates other
-              classes.
-            cat_mapping_old (dict[str, int]): dictionary that maps old class
-              names to their class ids.
-            cat_mapping_new (dict[str, int]): dictionary that maps updated
-              class names to their updated class ids.
-
-        Returns:
-            np.ndarray: updated image.
-        """
-        for class_name in classes_names_to_aggregate:
-            seg_map[seg_map == cat_mapping_old[class_name]] = cat_mapping_new[
-                super_class_name
-            ]
-        return seg_map
-
-    def get_roi_tokens(
-        self, path_of_dataset: str, roi: str, separator: str = "_"
-    ) -> tuple[str, str]:
-        """Constructs file and folder name from roi.
-
-        Args:
-            path (str): path of the dataset.
-            roi (str): name of the region of interest.
-            separator (str, optional): separator. Defaults to "_".
-
-        Returns:
-            (str, str): paths of the sample and its corresponding segmentation
-              map.
-        """
-        # Folder Name
-        roi_folder = separator.join(["S2"] + roi.split(separator)[:-1])
-        # File Name
-        roi_name = separator.join(["S2"] + roi.split(separator))
-        # Sample path
-        roi_file_path = os.path.join(
-            path_of_dataset, "patches", roi_folder, roi_name + ".tif"
-        )
-        # Segmentation map path
-        roi_file_cl_path = os.path.join(
-            path_of_dataset, "patches", roi_folder, roi_name + "_cl.tif"
-        )
-        return roi_file_path, roi_file_cl_path
-
-
-###############################################################
-# Weighting Function for Semantic Segmentation                #
-###############################################################
-def gen_weights(class_distribution, c=1.02):
-    return 1 / torch.log(c + class_distribution)
+        
