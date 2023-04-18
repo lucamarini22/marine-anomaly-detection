4456764471ed0aa1f6da07f9a7a2c77888c60711
Luca Marini <lucamarini1998@gmail.com>
2023-04-18 09:10:01 +0200

    add docstring

--- anomalymarinedetection/dataset/categoryaggregation.py
+++ anomalymarinedetection/dataset/categoryaggregation.py
@@ -2,5 +2,10 @@ from enum import Enum
 
 
 class CategoryAggregation(Enum):
+    """Enumerates the different types of aggregation of categories."""
+
     BINARY = "binary"
+    """Binary category aggregation: 'Marine Debris', 'Other'."""
     MULTI = "multi"
+    """Multi category aggregation,: 'Marine Debris', 
+    'Algae/Natural Organic Material', 'Ship', 'Clouds', 'Marine Water'."""
