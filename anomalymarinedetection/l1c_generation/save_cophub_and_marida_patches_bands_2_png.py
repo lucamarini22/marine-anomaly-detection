9d79c5e6b6948cfc66745644d6bb6b4ed97fa33e
Luca Marini <lucamarini1998@gmail.com>
2023-05-09 09:05:05 +0000

    update tiles

--- anomalymarinedetection/l1c_generation/save_cophub_and_marida_patches_bands_2_png.py
+++ anomalymarinedetection/l1c_generation/save_cophub_and_marida_patches_bands_2_png.py
@@ -20,8 +20,7 @@ from anomalymarinedetection.imageprocessing.float32_to_uint8 import (
     float32_to_uint8,
 )
 
-# TODO: Remove this
-TEMP = ["S2_24-3-20_18QYF", "S2_29-8-17_51RVQ"]
+MISSING_TILES = ["S2_12-1-17_16PCC", "S2_12-1-17_16PEC", "S2_21-2-17_16PCC"]
 
 
 def save_marida_and_cop_hub_2_png(
@@ -79,7 +78,7 @@ def save_marida_and_cop_hub_2_png(
         # TODO: remove this if condition asa you have all the cop hub images,
         # but keep the appending to list (outside the if condition when you
         # remove it)
-        if marida_patch_folder_name in TEMP:
+        if marida_patch_folder_name not in MISSING_TILES:
             marida_patch_name = remove_extension_from_name(
                 marida_patch_name, ext
             )
