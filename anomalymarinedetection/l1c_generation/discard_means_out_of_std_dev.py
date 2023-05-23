f665633702d79a5be2938de960fb7c241ef320b5
Luca Marini <lucamarini1998@gmail.com>
2023-05-23 07:27:40 +0000

    keep diffs if their std = 0

--- anomalymarinedetection/l1c_generation/discard_means_out_of_std_dev.py
+++ anomalymarinedetection/l1c_generation/discard_means_out_of_std_dev.py
@@ -10,17 +10,17 @@ def discard_means_out_of_std_dev(
     the interval [mean_diff - std_dev, mean_diff + std_dev].
 
     Args:
-        diffs_x (list[float]): list of differences.
-        mean_diffs_x (float): mean of differences.
-        std_dev_diffs_x (float): standard deviation of differences.
+        diffs (list[float]): list of differences.
+        mean_diffs (float): mean of differences.
+        std_dev_diffs (float): standard deviation of differences.
 
     Returns:
         np.ndarray: array of differences whose value is in
     the interval [mean_diff - std_dev, mean_diff + std_dev].
     """
-    assert std_dev_diffs > 0
+    assert std_dev_diffs >= 0
     differences = np.array(diffs)
-    less_than_std_away = np.abs(differences - mean_diffs) < std_dev_diffs
+    less_than_std_away = np.abs(differences - mean_diffs) <= std_dev_diffs
     differences = differences[less_than_std_away]
 
     return differences
