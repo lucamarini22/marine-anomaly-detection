0d911da140e7e3f9af35e195df26d9f33a5657e7
Luca Marini <lucamarini1998@gmail.com>
2023-06-02 08:52:47 +0000

    add exception if patch path does not exist

--- marineanomalydetection/io/load_roi.py
+++ marineanomalydetection/io/load_roi.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import os
 
 def load_roi(path: str) -> np.ndarray:
     """Loads region of interests from a .txt file into a numpy array.
@@ -7,7 +7,13 @@ def load_roi(path: str) -> np.ndarray:
     Args:
         path (str): path of the .txt file.
 
+    Raises:
+        FileNotFoundError: raises exception if the path of patch does not 
+          exist.
+    
     Returns:
         np.ndarray: Names of the regions of interests.
     """
+    if not os.path.isfile(path):
+        raise FileNotFoundError("The patch does not exist.")
     return np.genfromtxt(path, dtype="str")
